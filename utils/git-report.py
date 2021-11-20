dc2a4cc385e3eb572e1da0e2040d203178ece29d
acronim <marinko.miskovic.hr@gmail.com>
2021-11-20 23:32:35 +0100

    git report improvment

--- utils/git-report.py
+++ utils/git-report.py
@@ -4,29 +4,29 @@ import subprocess
 import os
 import argparse
 
-# Create the parser and add arguments
-parser = argparse.ArgumentParser()
-parser.add_argument(dest='author', type=str, help="github username")
-parser.add_argument(dest='directory', type=str,
-                    help="/full/path/to/the/repos/dir/")
-
-# Parse and print the results
-args = parser.parse_args()
-
-AUTHOR = args.author
-DIR_PATH = args.directory
-
-dir_paths = os.listdir(DIR_PATH)
+AUTHOR = ""
+BASE_DIR_PATH = ""
 
 repos = []
 
-def search_dirs():
-    for dir_path in dir_paths:
-        file = os.path.join(DIR_PATH, dir_path)
-        if(os.path.isdir(file)):
-            print(file)
-            if(os.path.exists(os.path.join(file, ".git"))):
-                repos.append(os.path.join(DIR_PATH, dir_path))
+
+def search_dirs(dir_paths, iteration):
+    iteration += 1
+
+    if(iteration <= 3):
+        next_level_list = []
+        for dir_path in dir_paths:
+            file = os.path.join(BASE_DIR_PATH, dir_path)
+            if(os.path.isdir(file)):
+                if(os.path.exists(os.path.join(file, ".git"))):
+                    repos.append(file)
+                else:
+                    next_level = []
+                    next_level += os.listdir(file)
+                    for nl in next_level:
+                        next_level_list.append(dir_path + "/" + nl)
+        if next_level_list:
+            search_dirs(next_level_list, iteration)
 
 
 def show_logs():
@@ -38,13 +38,9 @@ def show_logs():
 
         commits = subprocess.getstatusoutput(command)[1]
 
-        if(
-            len(commits) > 0 and
-            "Not a directory" not in commits and
-            "Not a git repository" not in commits
-        ):
+        if(len(commits) > 0):
 
-            print(os.path.relpath(dir, DIR_PATH))
+            print(os.path.relpath(dir, BASE_DIR_PATH))
 
             commits = [' '.join(['-'] + commit.split(' '))
                        for commit in commits.split('\n')]
@@ -52,5 +48,21 @@ def show_logs():
             print('\n'.join(commits) + '\n')
 
 
-search_dirs()
-show_logs()
+def main(dir_paths):
+    search_dirs(dir_paths, 0)
+    show_logs()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(dest='author', type=str, help="github username")
+    parser.add_argument(dest='directory', type=str,
+                        help="/full/path/to/the/repos/dir/")
+
+    args = parser.parse_args()
+
+    AUTHOR = args.author
+    BASE_DIR_PATH = args.directory
+
+    dir_paths = os.listdir(BASE_DIR_PATH)
+    main(dir_paths)
